b68fd04290c19c4ab1b337f97d6531be8b95fd28
Alexey Kuleshov <alexeykuleshov1990@gmail.com>
2016-04-25 10:29:50 +0200

    Add lake_example_002.py

--- examples/lake_example/lake_example_002.py
+++ examples/lake_example/lake_example_002.py
@@ -216,70 +216,70 @@ hds = fu.HeadFile(os.path.join(workspace, name+'.hds'))
 h = hds.get_data(kstpkper=(0, 0))
 
 x = y = np.linspace(0, L, N)
-c = plt.contour(x, y, h[-1], np.arange(90,100.1,0.2))
+c = plt.contour(x, y, h[0], np.arange(90,100.1,0.2))
 plt.clabel(c, fmt='%1.1f')
 plt.axis('scaled');
 plt.savefig(os.path.join(output, name+'_1.png'))
 plt.close()
 
 x = y = np.linspace(0, L, N)
-c = plt.contour(x, y, h[-2], np.arange(90,100.1,0.2))
+c = plt.contour(x, y, h[1], np.arange(90,100.1,0.2))
 plt.clabel(c, fmt='%1.1f')
 plt.axis('scaled');
 plt.savefig(os.path.join(output, name+'_2.png'))
 plt.close()
 
 x = y = np.linspace(0, L, N)
-c = plt.contour(x, y, h[-3], np.arange(90,100.1,0.2))
+c = plt.contour(x, y, h[2], np.arange(90,100.1,0.2))
 plt.clabel(c, fmt='%1.1f')
 plt.axis('scaled');
 plt.savefig(os.path.join(output, name+'_3.png'))
 plt.close()
 
 x = y = np.linspace(0, L, N)
-c = plt.contour(x, y, h[-4], np.arange(90,100.1,0.2))
+c = plt.contour(x, y, h[3], np.arange(90,100.1,0.2))
 plt.clabel(c, fmt='%1.1f')
 plt.axis('scaled');
 plt.savefig(os.path.join(output, name+'_4.png'))
 plt.close()
 
 x = y = np.linspace(0, L, N)
-c = plt.contour(x, y, h[-5], np.arange(90,100.1,0.2))
+c = plt.contour(x, y, h[4], np.arange(90,100.1,0.2))
 plt.clabel(c, fmt='%1.1f')
 plt.axis('scaled');
 plt.savefig(os.path.join(output, name+'_5.png'))
 plt.close()
 
 x = y = np.linspace(0, L, N)
-c = plt.contour(x, y, h[-6], np.arange(90,100.1,0.2))
+c = plt.contour(x, y, h[5], np.arange(90,100.1,0.2))
 plt.clabel(c, fmt='%1.1f')
 plt.axis('scaled');
 plt.savefig(os.path.join(output, name+'_6.png'))
 plt.close()
 
 x = y = np.linspace(0, L, N)
-c = plt.contour(x, y, h[-7], np.arange(90,100.1,0.2))
+c = plt.contour(x, y, h[6], np.arange(90,100.1,0.2))
 plt.clabel(c, fmt='%1.1f')
 plt.axis('scaled');
 plt.savefig(os.path.join(output, name+'_7.png'))
 plt.close()
 
 x = y = np.linspace(0, L, N)
-c = plt.contour(x, y, h[-8], np.arange(90,100.1,0.2))
+c = plt.contour(x, y, h[7], np.arange(90,100.1,0.2))
 plt.clabel(c, fmt='%1.1f')
 plt.axis('scaled');
 plt.savefig(os.path.join(output, name+'_8.png'))
 plt.close()
 
 x = y = np.linspace(0, L, N)
-c = plt.contour(x, y, h[-9], np.arange(90,100.1,0.2))
+c = plt.contour(x, y, h[8], np.arange(90,100.1,0.2))
 plt.clabel(c, fmt='%1.1f')
 plt.axis('scaled');
 plt.savefig(os.path.join(output, name+'_9.png'))
 plt.close()
 
 x = y = np.linspace(0, L, N)
-c = plt.contour(x, y, h[-10], np.arange(90,100.1,0.2))
+c = plt.contour(x, y, h[9], np.arange(90,100.1,0.2))
 plt.clabel(c, fmt='%1.1f')
 plt.axis('scaled');
 plt.savefig(os.path.join(output, name+'_10.png'))
