e2bb7b2b603577a8570cb4e693c29ceec29bd712
Alexey Kuleshov <alexeykuleshov1990@gmail.com>
2016-05-18 11:10:02 +0200

    performance_tests_overview
    performance_tests_overview

---
+++ performanceTest/command_list.sh.py
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+python 1_1-Gridsize.py 11
+python 1_1-Gridsize.py 101
+python 1_1-Gridsize.py 1001
+python 1_2-LayerNumbers.py 10
+python 1_2-LayerNumbers.py 100
+python 1_2-LayerNumbers.py 1000
