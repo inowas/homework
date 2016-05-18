ccebe7482ff1e3d9f03dcddb8178e33f5af76f13
Alexey Kuleshov <alexeykuleshov1990@gmail.com>
2016-05-18 11:29:25 +0200

    Command List

--- performanceTest/command_list.sh.py
+++ performanceTest/command_list.sh.py
@@ -6,3 +6,6 @@ python 1_1-Gridsize.py 1001
 python 1_2-LayerNumbers.py 10
 python 1_2-LayerNumbers.py 100
 python 1_2-LayerNumbers.py 1000
+python 1_2-Act/InactCells.py 25
+python 1_2-Act/InactCells.py 50
+python 1_2-Act/InactCells.py 75
