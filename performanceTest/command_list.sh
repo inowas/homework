888471d35689727d08c2c070ea46e0aabaff0345
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-05-30 13:30:18 +0200

    Addapt run-script

--- performanceTest/command_list.sh
+++ performanceTest/command_list.sh
@@ -1,48 +1,48 @@
 #! /bin/bash
 
-python 1_1-GridSize.py 11
-python 1_1-GridSize.py 101
-python 1_1-GridSize.py 1001
-python 1_2-LayerNumbers.py 10
-python 1_2-LayerNumbers.py 100
-python 1_2-LayerNumbers.py 1000
-python 1_3-InactActCells.py 0
-python 1_3-InactActCells.py 50
-python 1_3-InactActCells.py 100
-python 1_4-HydralicConductivities.py 0
-python 1_4-HydralicConductivities.py 100
-python 1_4-HydralicConductivities.py 10000
-python 1_5-StorageCapacities.py 0
-python 1_5-StorageCapacities.py 1
-python 1_5-StorageCapacities.py 2
-python 2_1-StressPeriodsNumber.py 1
-python 2_1-StressPeriodsNumber.py 50
-python 2_1-StressPeriodsNumber.py 100
-python 2_2-TimeStepsNumber.py 10
-python 2_2-TimeStepsNumber.py 100
-python 2_2-TimeStepsNumber.py 1000
-python 2_3-TimeStepsLength.py 1
-python 2_3-TimeStepsLength.py 100
-python 2_3-TimeStepsLength.py 10000
-python 2_4-RiverStage.py 0
-python 2_4-RiverStage.py 100
-python 2_4-RiverStage.py 1000
-python 2_5-RiverConductance.py 0
-python 2_5-RiverConductance.py 100
-python 2_5-RiverConductance.py 10000
-python 2_6-WellsNumbers.py 1
-python 2_6-WellsNumbers.py 2
-python 2_6-WellsNumbers.py 3
-python 2_7-PumpingRates.py 3 -100
-python 2_7-PumpingRates.py 3 -1000
-python 2_7-PumpingRates.py 3 -10000
-python 2_8-RechargeData.py 0
-python 2_8-RechargeData.py 10
-python 2_8-RechargeData.py 100
-python 2_9-GHB.py 0
-python 2_9-GHB.py 1
-python 3_1-Packages.py 1
-python 3_1-Packages.py 2
-python 3_2-Laycon.py 1 
-python 3_2-Laycon.py 2
-python 3_2-Laycon.py 3
+python -W ignore 1_1-GridSize.py 11
+python -W ignore 1_1-GridSize.py 101
+python -W ignore 1_1-GridSize.py 1001
+python -W ignore 1_2-LayerNumbers.py 10
+python -W ignore 1_2-LayerNumbers.py 100
+python -W ignore 1_2-LayerNumbers.py 1000
+python -W ignore 1_3-InactActCells.py 0
+python -W ignore 1_3-InactActCells.py 50
+python -W ignore 1_3-InactActCells.py 100
+python -W ignore 1_4-HydralicConductivities.py 0
+python -W ignore 1_4-HydralicConductivities.py 100
+python -W ignore 1_4-HydralicConductivities.py 10000
+python -W ignore 1_5-StorageCapacities.py 0
+python -W ignore 1_5-StorageCapacities.py 1
+python -W ignore 1_5-StorageCapacities.py 2
+#python -W ignore 2_1-StressPeriodsNumber.py 1
+#python -W ignore 2_1-StressPeriodsNumber.py 50
+#python -W ignore 2_1-StressPeriodsNumber.py 100
+python -W ignore 2_2-TimeStepsNumber.py 10
+python -W ignore 2_2-TimeStepsNumber.py 100
+python -W ignore 2_2-TimeStepsNumber.py 1000
+python -W ignore 2_3-TimeStepsLength.py 1
+python -W ignore 2_3-TimeStepsLength.py 100
+python -W ignore 2_3-TimeStepsLength.py 10000
+python -W ignore 2_4-RiverStage.py 0
+python -W ignore 2_4-RiverStage.py 100
+python -W ignore 2_4-RiverStage.py 1000
+python -W ignore 2_5-RiverConductance.py 0
+python -W ignore 2_5-RiverConductance.py 100
+python -W ignore 2_5-RiverConductance.py 10000
+python -W ignore 2_6-WellsNumbers.py 1
+python -W ignore 2_6-WellsNumbers.py 2
+python -W ignore 2_6-WellsNumbers.py 3
+python -W ignore 2_7-PumpingRates.py 3 -100
+python -W ignore 2_7-PumpingRates.py 3 -1000
+python -W ignore 2_7-PumpingRates.py 3 -10000
+python -W ignore 2_8-RechargeData.py 0
+python -W ignore 2_8-RechargeData.py 10
+python -W ignore 2_8-RechargeData.py 100
+python -W ignore 2_9-GHB.py 0
+python -W ignore 2_9-GHB.py 1
+python -W ignore 3_1-Packages.py 1
+python -W ignore 3_1-Packages.py 2
+python -W ignore 3_2-Laycon.py 1 
+python -W ignore 3_2-Laycon.py 2
+python -W ignore 3_2-Laycon.py 3
