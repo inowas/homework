75edf935280307a59807008b5fd796c799a3e445
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-07-07 11:56:30 +0200

    Add scenario_1 and results

---
+++ examples/pirna_model/scenario_1.py
@@ -0,0 +1,22 @@
+import os
+
+import matplotlib.pyplot as plt
+import flopy.modflow as mf
+import flopy.utils.formattedfile as ff
+import pprint
+
+workspace = os.path.join('scenario_1')
+pprint.pprint(workspace)
+
+modelname = 'Model_for_Pirna'
+
+ml = mf.Modflow.load(modelname+'.nam', exe_name='mf2005', model_ws=workspace, verbose=True)
+#ml.run_model()
+
+hdobj = ff.FormattedHeadFile(os.path.join(workspace, modelname+'.fhd'), precision='single')
+kstpkper = hdobj.get_kstpkper()
+
+resultsG21 = hdobj.get_ts((1, 5, 26))
+for result in resultsG21:
+    print "{};{}".format(result[0]/86400, result[1])
+
